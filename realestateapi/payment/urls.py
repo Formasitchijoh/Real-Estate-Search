45355a332ed447d5593b9a5fc36020fbeb1b6b91
Formasitchijoh <formasitf@gmail.com>
2024-07-05 10:37:58 +0100

    feat: complete implementation of payment using stripe

--- realestateapi/payment/urls.py
+++ realestateapi/payment/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PaymentAPI,CreatePaymentIntentView
+from .views import PaymentAPI,CreatePaymentIntentView, MakePayment,SaveStripeInfo
 
 urlpatterns = [
     path('make_payment/', PaymentAPI.as_view(), name='make_payment'),
-    path('payment/', CreatePaymentIntentView.as_view(), name='payment')
+    path('payment/', CreatePaymentIntentView.as_view(), name='payment'),
+    path('testpay/', MakePayment.as_view(), name='testpayment'),
+    path('stripe-user/', SaveStripeInfo.as_view(), name='stripe-user')
 ]
--- realestateapi/payment/views.py
+++ realestateapi/payment/views.py
@@ -6,8 +6,54 @@ import stripe
 from django.conf import settings
 
 
-stripe.api_key = settings.STRIPE_SECRET_KEY
+#stripe.api_key = settings.STRIPE_SECRET_KEY
+stripe.api_key = 'sk_test_51PYKmCRtxNTl8HYi2jyIKgbb6faoF8QIvcu0s8QJIh1ua4ntdjxh7KquP3qXpuLSroOnPKTCCnGa5VlxaYQG9qk800fbAhjpR6' # your real key will be much longer
 
+# payments/views.py
+class MakePayment(APIView):
+    def post(self,request):
+        test_payment_intent = stripe.PaymentIntent.create(
+            amount=1000, currency='pln', 
+            payment_method_types=['card'],
+            receipt_email='test@example.com')
+        return Response(status=status.HTTP_200_OK, data=test_payment_intent)
+
+class SaveStripeInfo(APIView):
+    def post(seld, request):
+        data = request.data
+        email = data['email']
+        payment_method_id = data['payment_method_id']
+        extra_msg = '' # add new variable to response message
+        # checking if customer with provided email already exists
+        customer_data = stripe.Customer.list(email=email).data   
+        
+        # if the array is empty it means the email has not been used yet  
+        if len(customer_data) == 0:
+            # creating customer
+            customer = stripe.Customer.create(
+            email=email, payment_method=payment_method_id)
+        else:
+            customer = customer_data[0]
+            extra_msg = "Customer already existed."
+            # add these lines
+            stripe.PaymentIntent.create(
+                customer=customer, 
+                amount=1500,
+                payment_method=payment_method_id,  
+                currency='pln', # you can provide any currency you want
+                confirm=True,
+                automatic_payment_methods={
+                        "enabled": True,
+                        "allow_redirects": "never",
+                    },
+                
+                )
+        return Response(status=status.HTTP_200_OK, data={
+            'message': 'Success', 
+            'data': {'customer_id': customer.id},
+            'extra_msg': extra_msg
+        }
+        ) 
 
 class CreatePaymentIntentView(APIView):
     def post(self, request):
